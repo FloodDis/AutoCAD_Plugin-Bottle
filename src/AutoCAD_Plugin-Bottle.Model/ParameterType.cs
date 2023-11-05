3d3d5eea6c30a3019e5561f9d4eb44a407ed099f
FloodDis <vlad.sechenov@mail.ru>
2023-11-06 03:48:24 +0700

    feature(ParameterType): создан класс ParameterType

---
+++ src/AutoCAD_Plugin-Bottle.Model/ParameterType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCAD_Plugin_Bottle.Model
+{
+	/// <summary>
+	/// Тип параметра.
+	/// </summary>
+	public enum ParameterType
+	{
+		/// <summary>
+		/// Длина основной части.
+		/// </summary>
+		Length = 0,
+
+		/// <summary>
+		/// Ширина основной части.
+		/// </summary>
+		Width,
+
+		/// <summary>
+		/// Высота основной части.
+		/// </summary>
+		MainHeight,
+
+		/// <summary>
+		/// Высота горлышка.
+		/// </summary>
+		NeckHeight,
+
+		/// <summary>
+		/// Радиус горлышка.
+		/// </summary>
+		NeckRadius
+	}
+}
